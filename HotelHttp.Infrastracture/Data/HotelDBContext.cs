fe4c18bc38a4661189ecd807dfc0bcfbcb891bc9
Denis Katasonov <diarusfoxs@gmail.com>
2025-05-12 15:01:18 +0300

    Add DbContext

---
+++ HotelHttp.Infrastracture/Data/HotelDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Hotel.Domain.Entitys;
+
+namespace Hotel.Infrastracture.Data
+{
+    internal class HotelDBContext : DbContext
+    {
+        public HotelDBContext(DbContextOptions<HotelDBContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<User> Users {  get; set; }
+    }
+}
