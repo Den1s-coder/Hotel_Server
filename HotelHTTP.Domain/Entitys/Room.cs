205ac496eca053977f95a09c8cc57f4d383f0d58
Denis Katasonov <diarusfoxs@gmail.com>
2025-05-12 15:22:47 +0300

    Change room entiry

--- HotelHTTP.Domain/Entitys/Room.cs
+++ HotelHTTP.Domain/Entitys/Room.cs
@@ -8,5 +8,8 @@ namespace Hotel.Domain.Entitys
 {
     public class Room
     {
+        public int Id { get; set; }
+        public string Number { get; set; } = string.Empty;
+        public bool IsAvailable { get; set; }
     }
 }
