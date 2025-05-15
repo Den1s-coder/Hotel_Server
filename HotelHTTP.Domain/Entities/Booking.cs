6f980b8b6ba8a157b90edd1fde5fa31b89a3a0f8
Denis Katasonov <diarusfoxs@gmail.com>
2025-05-15 10:29:22 +0300

    Update Entities for migrations

--- HotelHTTP.Domain/Entities/Booking.cs
+++ HotelHTTP.Domain/Entities/Booking.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hotel.Domain.Entitys
+namespace Hotel.Domain.Entities
 {
     public class Booking
     {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public decimal Price { get; set; }
+
+        public void UpdateDates(DateTime newCheckIn, DateTime newCheckOut, decimal pricePerNight)
+        {
+            if (newCheckIn >= newCheckOut)
+                throw new ArgumentException("Invalid dates");
+
+            CheckInDate = newCheckIn;
+            CheckOutDate = newCheckOut;
+            Price = CalculateTotalPrice(pricePerNight);
+        }
+
+        private decimal CalculateTotalPrice(decimal pricePerNight)
+        {
+            int days = (CheckOutDate - CheckInDate).Days;
+            return days * pricePerNight;
+        }
     }
 }
--- HotelHTTP.Domain/Entities/User.cs
+++ HotelHTTP.Domain/Entities/User.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hotel.Domain.Entitys
+namespace Hotel.Domain.Entities
 {
     public class User 
     {
