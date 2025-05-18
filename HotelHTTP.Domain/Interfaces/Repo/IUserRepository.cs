56a86b1fc39b1d9eaaee25f38ef8057494eb0320
Denis Katasonov <diarusfoxs@gmail.com>
2025-05-18 11:35:12 +0300

    Add Repo folder

--- HotelHTTP.Domain/Interfaces/Repo/IUserRepository.cs
+++ HotelHTTP.Domain/Interfaces/Repo/IUserRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hotel.Domain.Interfaces
+namespace Hotel.Domain.Interfaces.Repo
 {
     public interface IUserRepository
     {
