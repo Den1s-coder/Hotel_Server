912029f0a8379d88e6c12b97edb740cc1eb004d1
Denis Katasonov <diarusfoxs@gmail.com>
2025-05-18 11:37:32 +0300

    Add Jwt Token generation service

---
+++ HotelHTTP.API/Service/TokenService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Hotel.API.Service
+{
+    public class TokenService 
+    {
+        private readonly IConfiguration _config;
+
+        public TokenService (IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateToken (string username)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Issuer"],
+                audience: _config["Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
