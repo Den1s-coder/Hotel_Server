fb5f3fc9b8aba80b71b085ef9855a981f3d84c55
Denis Katasonov <diarusfoxs@gmail.com>
2025-05-18 11:34:38 +0300

    Add hasher for paswords
    -add NugetPackeg BCrypt

---
+++ HotelHttp.Infrastracture/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using Hotel.Domain.Interfaces.Auth;
+
+namespace Hotel.Infrastracture
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Generate(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, passwordHash);
+        }
+    }
+}
