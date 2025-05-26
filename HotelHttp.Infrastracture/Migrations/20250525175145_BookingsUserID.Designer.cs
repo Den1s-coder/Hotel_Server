2b67d887366a3636869e8ef0896d018fd0d2bd4c
Denis Katasonov <diarusfoxs@gmail.com>
2025-05-26 10:27:09 +0300

    Add migrations for Booking

--- HotelHttp.Infrastracture/Migrations/20250525175145_BookingsUserID.Designer.cs
+++ HotelHttp.Infrastracture/Migrations/20250525175145_BookingsUserID.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Hotel.Infrastracture.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Hotel.Infrastracture.Migrations
 {
     [DbContext(typeof(HotelDBContext))]
-    partial class HotelDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250525175145_BookingsUserID")]
+    partial class BookingsUserID
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
