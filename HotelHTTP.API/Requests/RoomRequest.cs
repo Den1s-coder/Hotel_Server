e7a03f962ca5044375bb99ae7192d6537941dbc1
Denis Katasonov <diarusfoxs@gmail.com>
2025-05-28 16:02:40 +0300

    Add capacity to request

--- HotelHTTP.API/Requests/RoomRequest.cs
+++ HotelHTTP.API/Requests/RoomRequest.cs
@@ -5,6 +5,7 @@
         public string Number { get; set; } = string.Empty;
         public string Type { get; set; }
         public decimal PricePerNight { get; set; }
-        public IFormFile Image { get; set; }
+        public int Capacity { get; set; } = 1;
+        public IFormFile? Image { get; set; }
     }
 }
