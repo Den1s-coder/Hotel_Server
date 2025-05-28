b7f556c39549c069ffc9028482f75e419dc21097
Denis Katasonov <diarusfoxs@gmail.com>
2025-05-28 16:06:00 +0300

    Realise Update room, GetActivity and UpdatePrice
    -Add Update room.
    -Update GetActivity now check price and capacity.
    -Add Update price for Types method.

---
+++ HotelHttp.Infrastracture/Migrations/20250528121032_RoomCapasity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Infrastracture.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoomCapasity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Capacity",
+                table: "Rooms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacity",
+                table: "Rooms");
+        }
+    }
+}
